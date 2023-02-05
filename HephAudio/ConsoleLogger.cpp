322a44f486617b3fcde41f6ed51c38c9660b8629
ozgur@gaia.gen.tr <ozgur@gaia.gen.tr>
2023-02-05 16:33:30 +0300

    added logging for android (ConsoleLogger)

--- HephAudio/ConsoleLogger.cpp
+++ HephAudio/ConsoleLogger.cpp
@@ -1,13 +1,16 @@
 #include "ConsoleLogger.h"
 #include <stdio.h>
 #include <chrono>
+#if defined(__ANDROID__)
+#include <android/log.h>
+#endif
 
 namespace HephAudio
 {
 	void ConsoleLogger::Log(StringBuffer message, const char* logLevel)
 	{
-#if defined(_WIN32) || defined(__linux__)
-		
+#if defined(_WIN32) || (defined(__linux__) && ! defined(__ANDROID__))
+
 		if (message.GetStringType() == StringType::Normal)
 		{
 			printf("\u001b[%smHephAudio[%s]: \033[0m%s", logLevel, ConsoleLogger::CurrentTimeToString(StringType::Normal).c_str(), message.c_str());
@@ -17,11 +20,68 @@ namespace HephAudio
 			wprintf(L"\u001b[%smHephAudio[%s]: \033[0m%s", StringBuffer(logLevel, StringType::Wide).wc_str(), ConsoleLogger::CurrentTimeToString(StringType::Wide).wc_str(), message.wc_str());
 		}
 
+#elif defined(__ANDROID__)
+
+		if (message.GetStringType() == StringType::Normal)
+		{
+			if (logLevel == ConsoleLogger::info)
+			{
+				__android_log_print(ANDROID_LOG_INFO, "HephAudio", "%s", message.c_str());
+			}
+			else if (logLevel == ConsoleLogger::warning)
+			{
+				__android_log_print(ANDROID_LOG_WARN, "HephAudio", "%s", message.c_str());
+			}
+			else if (logLevel == ConsoleLogger::error)
+			{
+				__android_log_print(ANDROID_LOG_ERROR, "HephAudio", "%s", message.c_str());
+			}
+			else if (logLevel == ConsoleLogger::success)
+			{
+				__android_log_print(ANDROID_LOG_VERBOSE, "HephAudio", "%s", message.c_str());
+			}
+			else if (logLevel == ConsoleLogger::debug)
+			{
+				__android_log_print(ANDROID_LOG_DEBUG, "HephAudio", "%s", message.c_str());
+			}
+			else
+			{
+				__android_log_print(ANDROID_LOG_DEFAULT, "HephAudio", "%s", message.c_str());
+			}
+		}
+		else
+		{
+			if (logLevel == ConsoleLogger::info)
+			{
+				__android_log_print(ANDROID_LOG_INFO, "HephAudio", "%ls", message.wc_str());
+			}
+			else if (logLevel == ConsoleLogger::warning)
+			{
+				__android_log_print(ANDROID_LOG_WARN, "HephAudio", "%ls", message.wc_str());
+			}
+			else if (logLevel == ConsoleLogger::error)
+			{
+				__android_log_print(ANDROID_LOG_ERROR, "HephAudio", "%ls", message.wc_str());
+			}
+			else if (logLevel == ConsoleLogger::success)
+			{
+				__android_log_print(ANDROID_LOG_VERBOSE, "HephAudio", "%ls", message.wc_str());
+			}
+			else if (logLevel == ConsoleLogger::debug)
+			{
+				__android_log_print(ANDROID_LOG_DEBUG, "HephAudio", "%ls", message.wc_str());
+			}
+			else
+			{
+				__android_log_print(ANDROID_LOG_DEFAULT, "HephAudio", "%ls", message.wc_str());
+			}
+		}
+
 #endif
 	}
 	void ConsoleLogger::LogLine(StringBuffer message, const char* logLevel)
 	{
-#if defined(_WIN32) || defined(__linux__)
+#if defined(_WIN32) || (defined(__linux__) && ! defined(__ANDROID__))
 
 		if (message.GetStringType() == StringType::Normal)
 		{
--- HephAudio/StopWatch.cpp
+++ HephAudio/StopWatch.cpp
@@ -6,7 +6,7 @@ namespace HephAudio
 	void StopWatch::Start()
 	{
 		std::thread::id currentThreadId = std::this_thread::get_id();
-		if (!timePoints.contains(currentThreadId))
+		if (timePoints.find(currentThreadId) == timePoints.end())
 		{
 			timePoints[currentThreadId] = std::chrono::high_resolution_clock::now();
 		}
@@ -26,7 +26,7 @@ namespace HephAudio
 	void StopWatch::Stop()
 	{
 		std::thread::id currentThreadId = std::this_thread::get_id();
-		if (timePoints.contains(currentThreadId))
+		if (timePoints.find(currentThreadId) != timePoints.end())
 		{
 			timePoints.erase(currentThreadId);
 		}
