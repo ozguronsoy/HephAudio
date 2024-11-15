18aa32e0dc03c92b041e64df2debbad59548a515
Özgür <ozguronsoy@gmail.com>
2024-11-15 20:45:28 +0300

    added change sample rate effect class

---
+++ HephAudio/HeaderFiles/AudioEffects/ChangeSampleRateEffect.h
@@ -0,0 +1,63 @@
+#pragma once
+#include "HephAudioShared.h"
+#include "DoubleBufferedAudioEffect.h"
+
+/** @file */
+
+namespace HephAudio
+{
+	/**
+	 * @brief changes the sample rate.
+	 * 
+	 */
+	class ChangeSampleRateEffect : public DoubleBufferedAudioEffect
+	{
+	public:
+		using DoubleBufferedAudioEffect::Process;
+
+	protected:
+		/**
+		 * the sample rate the input will be converted to.
+		 * 
+		 */
+		size_t outputSampleRate;
+
+	public:
+		/** @copydoc default_constructor */
+		ChangeSampleRateEffect();
+
+		/**
+		 * @copydoc constructor
+		 *
+		 * @param outputSampleRate @copydetails outputSampleRate
+		 * 
+		 */
+		explicit ChangeSampleRateEffect(size_t outputSampleRate);
+
+		/** @copydoc destructor */
+		virtual ~ChangeSampleRateEffect() = default;
+
+		virtual std::string Name() const override;
+		virtual size_t CalculateRequiredFrameCount(size_t outputFrameCount, const AudioFormatInfo& formatInfo) const override;
+		virtual size_t CalculateOutputFrameCount(const AudioBuffer& buffer) const override;
+		virtual void Process(AudioBuffer& buffer, size_t startIndex, size_t frameCount) override;
+
+
+		/**
+		 * gets the output sample rate.
+		 * 
+		 */
+		virtual size_t GetOutputSampleRate() const;
+
+		/**
+		 * sets the output sample rate.
+		 * 
+		 * @param outputSampleRate @copydetails outputSampleRate
+		 * 
+		 */
+		virtual void SetOutputSampleRate(size_t outputSampleRate);
+
+	protected:
+		virtual void ProcessST(const AudioBuffer& inputBuffer, AudioBuffer& outputBuffer, size_t startIndex, size_t frameCount) override;
+	};
+}
---
+++ HephAudio/SourceFiles/AudioEffects/ChangeSampleRateEffect.cpp
@@ -0,0 +1,86 @@
+#include "AudioEffects/ChangeSampleRateEffect.h"
+#include "Exceptions/InvalidArgumentException.h"
+
+using namespace Heph;
+
+namespace HephAudio
+{
+	ChangeSampleRateEffect::ChangeSampleRateEffect() : ChangeSampleRateEffect(48000) {}
+
+	ChangeSampleRateEffect::ChangeSampleRateEffect(size_t outputSampleRate) : DoubleBufferedAudioEffect(), outputSampleRate(outputSampleRate) {}
+
+	std::string ChangeSampleRateEffect::Name() const
+	{
+		return "Change Sample Rate";
+	}
+
+	size_t ChangeSampleRateEffect::CalculateRequiredFrameCount(size_t outputFrameCount, const AudioFormatInfo& formatInfo) const
+	{
+		return ceil(outputFrameCount * ((double)formatInfo.sampleRate / this->outputSampleRate));
+	}
+
+	size_t ChangeSampleRateEffect::CalculateOutputFrameCount(const AudioBuffer& buffer) const
+	{
+		const AudioFormatInfo& formatInfo = buffer.FormatInfo();
+		if (formatInfo.sampleRate == 0)
+		{
+			HEPH_RAISE_AND_THROW_EXCEPTION(this, InvalidArgumentException(HEPH_FUNC, "input sample rate cannot be 0."));
+		}
+
+		return buffer.FrameCount() * ((double)this->outputSampleRate / formatInfo.sampleRate);
+	}
+
+	void ChangeSampleRateEffect::Process(AudioBuffer& buffer, size_t startIndex, size_t frameCount)
+	{
+		DoubleBufferedAudioEffect::Process(buffer, startIndex, frameCount);
+		buffer.SetSampleRate(this->outputSampleRate);
+	}
+
+	size_t ChangeSampleRateEffect::GetOutputSampleRate() const
+	{
+		return this->outputSampleRate;
+	}
+
+	void ChangeSampleRateEffect::SetOutputSampleRate(size_t outputSampleRate)
+	{
+		if (outputSampleRate == 0)
+		{
+			HEPH_RAISE_AND_THROW_EXCEPTION(this, InvalidArgumentException(HEPH_FUNC, "output sample rate cannot be 0."));
+		}
+
+		this->outputSampleRate = outputSampleRate;
+	}
+
+	void ChangeSampleRateEffect::ProcessST(const AudioBuffer& inputBuffer, AudioBuffer& outputBuffer, size_t startIndex, size_t frameCount)
+	{
+		const AudioFormatInfo& inputFormatInfo = inputBuffer.FormatInfo();
+		if (inputFormatInfo.sampleRate == 0)
+		{
+			HEPH_RAISE_AND_THROW_EXCEPTION(this, InvalidArgumentException(HEPH_FUNC, "input sample rate cannot be 0."));
+		}
+
+		const double srRatio = (double)this->outputSampleRate / (double)inputFormatInfo.sampleRate;
+
+		startIndex = startIndex * srRatio;
+		frameCount = frameCount * srRatio;
+		const size_t endIndex = startIndex + frameCount;
+
+		if (srRatio != 1)
+		{
+			for (size_t i = startIndex; i < endIndex; ++i)
+			{
+				const double resampleIndex = i / srRatio;
+				const double resampleFactor = resampleIndex - floor(resampleIndex);
+
+				for (size_t j = 0; j < inputFormatInfo.channelLayout.count; ++j)
+				{
+					outputBuffer[i][j] = inputBuffer[resampleIndex][j] * (1.0 - resampleFactor);
+					if ((resampleIndex + 1) < inputBuffer.FrameCount())
+					{
+						outputBuffer[i][j] += inputBuffer[resampleIndex + 1][j] * resampleFactor;
+					}
+				}
+			}
+		}
+	}
+}
